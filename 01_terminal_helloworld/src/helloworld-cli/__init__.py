9977805e4b2de82bff60ca4d174ad979cd2fac8c
Christian Buhtz <c.buhtz@posteo.jp>
2023-11-13 14:29:29 +0100

    x

---
+++ 01_terminal_helloworld/src/helloworld-cli/__init__.py
@@ -0,0 +1,4 @@
+# This file is usualy empty.
+# It marks this folder as a "Python Import Package".
+# It is run when you "import" a package.
+# Further reading: https://stackoverflow.com/a/31329365/4865723
