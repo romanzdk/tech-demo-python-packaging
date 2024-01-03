a08a41ef40427f734dbc520eefd77da08f192d0f
Christian Buhtz <c.buhtz@posteo.jp>
2024-01-03 10:13:45 +0100

    inject user path into root environment

--- 04_as_user_and_root/src/rootuser/__main__.py
+++ 04_as_user_and_root/src/rootuser/__main__.py
@@ -9,21 +9,26 @@ import rootusergui
 def run_main_as_root_via_policykit():
     """Start this applicaton again in a new process with root privilegs using
     pkexec.
-
-    STATUS: Currently this does not work for unknown reasons.
     """
-    cmd = ['pkexec', #'--disable-internal-agent',
-           # This two environment variables need to be set to make root
-           # able to run GUI applications.
+
+    # Assuming the package is installed as user only it can not be found in
+    # roots Pythons search path. The users Python search path is injected in
+    # roots environment via $PYTHONPATH.
+    user_sitepackages_path = __file__.rsplit('rootusergui')[0]
+    python_path = os.environ.get('PYTHONPATH', '')
+    python_path = f'{python_path}:{user_sitepackages_path}'
+
+    cmd = ['pkexec',
            'env',
            f'DISPLAY={os.environ["DISPLAY"]}',
-           # f'XAUTHORITY={os.environ["XAUTHORITY"]}',
+           f'XAUTHORITY={os.environ["XAUTHORITY"]}',
+           f'PYTHONPATH={python_path}',
            # In most cases this is /usr/local/bin/bitgui which is not in PATH
            # of pkexec environments.
            shutil.which('usergui')
            ]
 
-    print(f'Try to execute {cmd=}.')
+    print(f'FOO Try to execute {cmd=}.')
 
     # See https://github.com/python/cpython/issues/39569
     os.execvp(cmd[0], cmd)
